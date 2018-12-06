6fea62298daceefa298bc1cee3ebe666cdc9e81f
John Foley <jjfoley@smith.edu>
2018-12-06 16:00:19 +0000

    trying update.sh

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ set -eu
 
 CHANGES=`git pull`
 echo "Changes=$CHANGES"
-if [ 0 -ne $CHANGES ]; then
+if [[ "Updating"* == $CHANGES ]]; then
   echo "Remote changes!"
   echo "The last word is now! $(date)" > timestamp
   git add timestamp
